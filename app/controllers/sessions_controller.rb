07ea18414276a3192698f45cc9d8a4ce82982112
Katherine Thongsakounh & Brian Lin <pair+kthong3.lin-brian-l@devbootcamp.com>
2017-10-09 11:37:16 -0500

    Create login functionality.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if !@user.nil? && !!@user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      @user = User.new(email: params[:email])
+      @error = true
+      render "new"
+    end
+  end
+
+end
