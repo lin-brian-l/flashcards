68ef92670a72511fd05194a031278da63a832ba8
Katherine Thongsakounh & Brian Lin <pair+kthong3.lin-brian-l@devbootcamp.com>
2017-10-09 11:03:39 -0500

    Create homepage and signup page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :decks, :sessions, :users
+
+  root "decks#index"
+
+  get '/' => 'decks#index'
+
+  get '/register', to: 'users#new', as: :register
+
+  get '/login', to: 'sessions#new', as: :login
+
 end
